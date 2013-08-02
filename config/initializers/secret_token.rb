7b039c78a5316f718a8731023196364240bd7abf
mosin <otmosina@gmail.com>
2013-08-02 15:00:35 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basketapp::Application.config.secret_token = '4928c1c06016e628e559c79591f865e2af5f9b6d8c2d579c6f5cf14cf3969731e8923f3129937c968a679725050491b1761c627f092847629cfab2e3cb53afdf'
